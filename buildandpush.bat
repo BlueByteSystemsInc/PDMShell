1f3647978a72bee50ba0e3c8e326031aac7cb9e4
Amen Jlili <amen+tei@bluebyte.biz>
2026-07-19 12:20:11 -0700

    updated docs

--- buildandpush.bat
+++ buildandpush.bat
@@ -2,9 +2,9 @@
 powershell -ExecutionPolicy Bypass -File .\postbuild.ps1
 .\bin\docfx.exe .\website.json
 powershell -ExecutionPolicy Bypass -File .\postbuild_website.ps1
-if exist ".\deploy-ftp.local.ps1" powershell -ExecutionPolicy Bypass -File ".\deploy-ftp.local.ps1"
 git add .
 git commit -m "updated docs"
 git push 
 cd website 
 start .
+if exist ".\deploy-ftp.local.ps1" powershell -ExecutionPolicy Bypass -File ".\deploy-ftp.local.ps1"
